31a9bf2e661b3328814e9c3c53953de6be250b36
haider <headeralishah@gmail.com>
2020-03-15 14:26:29 +0500

    class code Hello world in JavaScript

---
+++ JavaScript/Hello JavaScript/script.js
@@ -0,0 +1,82 @@
+/*
+alert(20204);
+
+alert("Welcome Haider");
+
+alert("03413542800");
+*/
+
+// console.log(5000);
+// console.log("Welcome Haider");
+
+
+
+
+
+
+
+
+
+
+// var abc = "haider ali shah";
+// var xyz = "abc";
+// console.log(abc);
+// console.log(abc);
+// console.log("abc");
+// console.log("xyz");
+// console.log(xyz);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var invoice = 2000;
+// var bonus = 500;
+// var sum = invoice + bonus;
+// console.log(invoice + bonus);
+// console.log(invoice - bonus);
+// console.log(invoice * bonus);
+// console.log(invoice / bonus);
+// console.log(5000 + 2000 - 500 * 10 / 5);
+// console.log(sum);
+
+
+
+
+
+console.log("haider" + " " + "ali");
+//haider ali
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
