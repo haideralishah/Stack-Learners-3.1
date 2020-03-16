c31eaf5eb5c9b576ebc92ce27bf0323e9ec74ab0
haider <headeralishah@gmail.com>
2020-03-16 14:40:58 +0500

    class code till 15th March

--- JavaScript/Hello JavaScript/script.js
+++ JavaScript/Hello JavaScript/script.js
@@ -1,14 +1,13 @@
-/*
-alert(20204);
 
-alert("Welcome Haider");
+// alert(20204);
 
-alert("03413542800");
-*/
+// alert("Welcome Haider");
+
+// alert("03413542800");
+
 
 // console.log(5000);
 // console.log("Welcome Haider");
-
 
 
 
@@ -56,8 +55,9 @@ alert("03413542800");
 
 
 
-
-console.log("haider" + " " + "ali");
+// console.log("haider" + "ali");
+// //haiderali
+// console.log("haider" + " " + "ali");
 //haider ali
 
 
@@ -67,6 +67,171 @@ console.log("haider" + " " + "ali");
 
 
 
+
+
+
+
+// var firstName = "Haider";
+// var lastName = "Ali";
+// var fullName = firstName + " " + lastName;
+// //HaiderAli
+// //Haider Ali
+// console.log(fullName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This is "JavaScript" class.
+//This is 'JavaScript' "class".
+
+// console.log("This is 'JavaScript' " +  '"class."');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fullName;
+// console.log(fullName);
+// fullName = "alex";
+// console.log(fullName);
+
+
+
+
+
+// var firstName, lastName, fullName;
+// console.log(firstName, lastName, fullName);
+// lastName = 'pope';
+// firstName = 'alex';
+// fullName = firstName + " " +  lastName ;
+
+// console.log(firstName, lastName, fullName);
+
+// var fathersName = "";
+// console.log(fathersName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var sum = 500;
+// console.log(sum);
+
+// sum = 400;
+// console.log(sum);
+
+// sum = 'it is string';
+
+// console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var message;
+// message = "hello world";
+// alert(message);
+
+
+
+// var age = 14;
+// console.log("I am " + age + " years old.");
+
+
+
+// console.log('Haider\nali');
+// alert('Haider\nali');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Pizza\nPizz\nPiz\nPi\nP");
 
 
 
