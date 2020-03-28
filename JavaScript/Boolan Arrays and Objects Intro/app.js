fc1e6ea3b550afff696808154cc8f94aa2b25aee
haider <headeralishah@gmail.com>
2020-03-28 19:02:05 +0500

    object loop practice and functions intro

--- JavaScript/Boolan Arrays and Objects Intro/app.js
+++ JavaScript/Boolan Arrays and Objects Intro/app.js
@@ -159,15 +159,15 @@ array
 
 // var cities = ["Karachi", "Lahore", "Quetta", "Islamabad", "Peshawar", "Multan"];
 
-var car = {
-    carName: "cyber truck", 
-    manufacturer: "tesla" ,
-    isElectric: true, 
-    price: 60000
-};
+// var car = {
+//     carName: "cyber truck",
+//     manufacturer: "tesla",
+//     isElectric: true,
+//     price: 60000
+// };
 
-var stdInfo = {};
-stdInfo.userName = 'abdullah';
+// var stdInfo = {};
+// stdInfo.userName = 'abdullah';
 // console.log(car);
 // console.log(car.carName);
 // console.log(car.price);
@@ -175,18 +175,230 @@ stdInfo.userName = 'abdullah';
 // car.price = 500000;
 // console.log(car.price);
 
-car.ownerName = 'Haider';
+// car.ownerName = 'Haider';
 
-console.log(car);
+// console.log(car);
 
-console.log(stdInfo);
-
-
+// console.log(stdInfo);
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pakistan mai 4 sobe hn 
+// parlimantry system h 
+// sobo k capitals karachi lahore peshawar and quetta hn
+// area 996960 square km h
+
+
+// var country = {
+//     name: "Pakistan",
+//     province: 4,
+//     system: 'parlimentry'     ,
+//     // isParlimentrySystem: true,
+//     provinceCapital: ["Karachi", "Lahore", "Peshawar", "Quetta"],
+//     area: 996960,
+//     areaUnit: 'sq km'
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// patien name Akram Khan
+// symptoms fever, cough flue
+// corona nahi h
+// isolation start noi hoi
+
+
+// patien name Aslam Khan
+// symptoms fever, cough flue
+// corona h
+// isolation start hogi
+
+// patien name Munshi Khan
+// symptoms cough flue
+// corona nahi h
+// isolation start noi hoi
+
+// var patient1 = {
+//     name: "Akram Khan",
+//     symptoms: ['fever', 'cough', 'flue'],
+//     corona: false,
+//     isolation: false,
+// }
+
+// var patient2 = {
+//     name: 'Aslam Khan',
+//     symptoms: ['fever', 'cough', 'flue'],
+//     corona: true,
+//     isolation: true
+// }
+
+
+// var patient3 = {
+//     name: 'Munshi Khan',
+//     symptoms: ['cough', 'flue'],
+//     corona: false,
+//     isolation: false
+// }
+// var patients = [{
+//     name: "Akram Khan",
+//     symptoms: ['fever', 'cough', 'flue'],
+//     corona: false,
+//     isolation: false,
+// },
+// {
+//     name: 'Aslam Khan',
+//     symptoms: ['fever', 'cough', 'flue'],
+//     corona: true,
+//     isolation: true
+// },
+// {
+//     name: 'Munshi Khan',
+//     symptoms: ['cough', 'flue'],
+//     corona: false,
+//     isolation: false
+// }];
+
+// var patients = [patient1, patient2, patient3]
+// console.log(patients);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // user name
+// // roll Number
+// // subject 1
+// // subject 2
+// var studentName = prompt('whats your name?');
+// var rollNumber = prompt('enter roll number');
+// // var subject1 = prompt('enter primary subject');
+// // var subject2 = prompt('enter optional subject');
+
+// var subjects = [];
+// subjects[0] = prompt('enter primary subject');
+// subjects[1] = prompt('enter optional subject');
+
+// // var subjects = [prompt('enter primary subject'), prompt('enter optional subject')];
+
+// var student = {
+//     name: studentName,
+//     rollNumber: rollNumber,
+//     // subjects: [subject1, subject2]
+//     subjects: subjects
+// }
+
+// console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ask 2 numbers and console sum of them
+
+// var num1 = +prompt('enter first number');
+// var num2 = +prompt('enter second number');
+// console.log(num1 + num2);
+
+
+
+
+// ask user his 3 hobies
+
+// var hobby1 = prompt('enter your first hobby');
+// var hobby2 = prompt('enter your second hobby');
+// var hobby3 = prompt('enter your thir hobby');
+
+// var hobbies = [hobby1, hobby2, hobby3];
+
+// console.log(hobbies);
 
 
 
---
+++ JavaScript/Functions/app.js
@@ -0,0 +1,26 @@
+/*********Functions *********/
+
+// function cookFood(){
+//     console.log('food is ready');
+// }
+
+
+// cookFood();
+// cookFood();
+// cookFood();
+// cookFood();
+
+
+
+
+
+
+function sum2Numbers(){
+    var num1 = +prompt('enter a number');
+    var num2 = +prompt('enter another number');
+    console.log(num1 + num2);
+}
+
+sum2Numbers();
+sum2Numbers();
+sum2Numbers();
