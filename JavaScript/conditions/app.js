02851f63eabd350cb22c8fa8129415e5c2dae8ea
haider <headeralishah@gmail.com>
2020-04-04 19:13:06 +0500

    conditions

---
+++ JavaScript/conditions/app.js
@@ -0,0 +1,297 @@
+// strings
+// numbers
+// boolean
+// arrays
+// objects
+// functions
+// mathmetical operations 
+// prompt alert and console.log
+// var
+// onclick
+// document.getElementById()
+// document.getElementById().innerHTML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt('what is your name?');
+
+// var h1 = document.getElementById('greet-user');
+
+// h1.innerHTML = 'Welcome ' + userName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function userMessage(){
+//     var message = prompt('write any message');
+//     var p = document.getElementById('message');
+//     p.innerHTML = message;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = 20;
+
+// // console.log(age > 14);
+
+// if(age > 14){
+//     console.log('You are elegible for classes.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = +prompt('what is your age?')
+
+// // console.log(age > 14);
+
+// if(age > 14){
+//     console.log('You are elegible for classes.');
+// }
+// else{
+//     console.log('Your are not elegible for classes. \nApply once you are 14 years old.')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function checkAge(){
+//     var age = +prompt('what is your age?'); 
+//     var p = document.getElementById("eligibility-msg");
+//     if(age >= 14){
+//         p.innerHTML = 'You are elegible for classes.';
+//     }
+//     else{
+//         p.innerHTML = 'You are not elegible for classes. <br /> Please apply once you are 14 years old.'
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <
+// >
+// <=
+// >=
+// ===
+// !==
+
+
+// console.log(14 > 20); // false
+// console.log(14 > 10);  // true
+// console.log(14 > 14);  // false
+
+// console.log(14 < 20);  //true
+// console.log(14 < 10);  //false
+// console.log(14 < 14);  // false
+
+// console.log(14 >= 14); // true
+// console.log(14 >= 15);  //false
+
+// console.log(14 <= 14); // true
+// console.log(14 <= 13); // false
+
+
+
+
+
+// var age = +prompt('what is your age?');
+
+// if(age === 16){
+//     console.log('You are elegible.');
+// }
+
+
+
+
+// var allowedRelegion = "Islam";
+// var userRelegion = prompt('what is your relegion?');
+
+// if(userRelegion !== allowedRelegion){
+//     console.log('Sorry! you are not allowed.');
+// }
+// else{
+//     console.log('Congratulations! You got visa');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(2 - 2 === 4){
+//     console.log('insid if');
+// }
+
+
+
+
+
+if(10 - 5 > 3){
+    console.log('number is divisble by 5');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
