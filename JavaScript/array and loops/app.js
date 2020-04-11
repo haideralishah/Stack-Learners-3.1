259f4b4d99146e8ab10955f22d6aaca319e4936d
haider <headeralishah@gmail.com>
2020-04-11 20:21:48 +0500

    arrays methods

---
+++ JavaScript/array and loops/app.js
@@ -0,0 +1,147 @@
+// DOM document.getElementById()
+// Data Types
+// On Click
+// if else if else
+// comparision operators
+// functions
+
+/*
+array methods
+pop
+push
+
+shift
+unshift
+
+splice
+indexOf
+
+array properties
+.length
+
+*/
+
+
+
+
+// var cities = ['Karachi', 'Lahore', 'Islamabad'];
+
+// console.log(cities.length);
+
+
+// // add at the end of array
+// cities.push("Peshawar");
+// cities.push("Quetta");
+
+// // add at the begining of array
+// cities.unshift('Hyderabad');
+// cities.unshift('Multan');
+
+// // console.log(cities);
+
+
+// // delete last index
+// cities.pop();
+// // console.log(cities);
+
+// // delete first index
+// cities.shift();
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//indexOf tels index of item
+// if not exist -1 will be return
+// if 2 elements exists then first index will be return
+
+
+
+// var fruits = ['Apple', 'Banana', 'Grapes', 'Apple', 'Orange', 'Avocod', 'Date'];
+
+// var avocodIndex = fruits.indexOf("Apple");
+// console.log(avocodIndex)
+
+// if(avocodIndex !== -1){    
+//     fruits.splice(avocodIndex, 1);
+// }
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['Apple', 'Banana', 'Orange', 'Avocod', 'Date'];
+
+// fruits.splice(2, 2, "Grapes", "Watermelon", "Mango");
+// console.log(fruits);
+
+
+
+
+
+// var fruits = ['Apple', 'Banana', 'Orange', 'Avocod', 'Date'];
+
+// fruits.splice(1, 0, "Grapes", "Watermelon", "Mango");
+// fruits.splice(6, 1);
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
