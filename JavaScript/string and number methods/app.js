f881a0d1c673a493caf1b0a3a10050d1da89db9e
haider <headeralishah@gmail.com>
2020-04-19 19:02:30 +0500

    loops and string methods

---
+++ JavaScript/string and number methods/app.js
@@ -0,0 +1,319 @@
+/*
+string
+number
+boolean
+object
+array
+function
+conditions
+loops
+document.getElementById().innherHTML
+
+
+alert()
+prompt()
+console.log()
+
+mathemetical operations
+
+arrays methods
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+// var tableName = +prompt('which table you want to learn?');
+// var count = +prompt('how many times you want table to be printed');
+
+// for(var i =1; i <= count; i++){
+//     console.log(tableName + ' x ' + i + ' = ' + i * tableName);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5 rounds
+// 2 glass of water
+// 10 pushups
+
+
+
+// for(var rounds = 1; rounds <= 5; rounds++){
+//     console.log('round ' + rounds + ' complete.');
+//     for(var glassOfWater = 1; glassOfWater <= 2; glassOfWater++){
+//         console.log(glassOfWater + ' glass of water drunk');
+//         for(var pushups = 1; pushups <= 10; pushups++){
+//             console.log(pushups + ' pushups done')
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(var i =0; i< 5 ; i++){
+//     document.write('<h1>*****</h1>');
+// }
+
+
+// var str = '*'
+// for(var i =0; i< 5 ; i++){
+//     document.write("<h1>" + str + "</h1>");
+//     str = str + '*';
+// }
+
+
+
+
+
+// var str = '********'
+// for (var i = 1; i < 9; i++) {
+//     document.write("<h1>" + str + "</h1>");
+    
+//     str = str.slice(0,7);
+    
+//     // str = str.slice(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['apple', 'bnana', 'grapes', 'pineapple', 'watermelon', 'orange', 'kiwi', 'guava'];
+
+// var favFruits = fruits.slice(3,6);
+// console.log(favFruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+.length
+
+.splice()
+.push()
+.pop()
+.shift()
+.unshift()
+.indexOf()
+
+.slice()
+
+*/
+
+
+
+// var userName = 'Haider Ali Shah'
+// console.log(userName.length);
+// var sirName = userName.slice(11);
+// console.log(sirName);
+// var secondName = userName.slice(7,10);
+// console.log(secondName);
+
+
+
+
+
+
+
+
+
+
+/*
+.toLowerCase()
+.toUpperCase()
+
+*/
+
+// var userName = prompt('what is your name?');
+// userName.toUpperCase()
+// userName.length;
+
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+
+
+
+
+
+
+
+// print city name in title case.
+
+// var cityName = prompt('what is your city name?'); //lahore
+// var firstChar = cityName[0]; // l
+// var otherChar = cityName.slice(1); //ahore
+
+
+// var cityNameInTitleCase = firstChar.toUpperCase() + otherChar.toLowerCase();
+
+// console.log(cityNameInTitleCase);
+
+
+
+
+
+// print full para in title case.
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. In asperiores quam qui quasi odio assumenda explicabo magnam quae, ex numquam exercitationem odit neque nostrum aliquid voluptates. Labore perspiciatis at neque.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cleanstCity = ['karachi', 'Lahore', 'IslamaBad', 'qUeTTA'];
+
+// var cityName = prompt('where do you live in?');
+
+// var cleanCity = false;
+
+// for (var i = 0; i < cleanstCity.length; i++) {
+//     if (cityName.toLowerCase() === cleanstCity[i].toLowerCase()) {
+//         cleanCity = true;
+//         break;
+//     }
+// }
+
+// if (cleanCity) {
+//     console.log('you live in clean city')
+// }
+// else {
+//     console.log('you dont live in clean city')
+// }
+
+
+
+
+
+
+
