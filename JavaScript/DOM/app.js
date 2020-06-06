d098ddd222b5930ecf6b30ec045f57a210eb3afc
Haider Ali Shah <headeralishah@gmail.com>
2020-06-06 19:51:54 +0500

    Events finished DOM started

---
+++ JavaScript/DOM/app.js
@@ -0,0 +1,116 @@
+
+
+// var secondLastP = document.getElementById('second-last');
+
+// console.log(secondLastP)
+
+// secondLastP.innerHTML = 'short text'
+
+
+
+
+// var allParagraphs = document.getElementsByTagName('p');
+
+// console.log(allParagraphs);
+
+
+// console.log(allParagraphs[5].id);
+
+// for (var i = 0; i < allParagraphs.length; i++) {
+//     // console.log(allParagraphs[i]);
+//     // var paraInput = prompt('what do you want to show in this para?');
+
+//     allParagraphs[i].innerHTML = 'This is ' + i + " paragraph."
+
+
+//     // allParagraphs[i].innerHTML = paraInput;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var divEl = document.getElementById('other-para');
+
+// var allParagraphsInDiv = divEl.getElementsByTagName('p');
+
+// console.log(allParagraphsInDiv);
+
+// allParagraphsInDiv[1].style.backgroundColor = 'grey';
+// allParagraphsInDiv[1].style.color = 'yellow';
+// allParagraphsInDiv[1].innerHTML = 'A quick brown fox jumps over the lazy dog.'
+
+
+
+
+
+
+
+
+
+// var specialEl = document.getElementsByClassName('special');
+
+// console.log(specialEl);
+
+// specialEl[0].innerHTML = 'hello DOM'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[2]);
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[1].childNodes[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+1 show 3 unfolded cards
+2 click on king 
+3 once user clicks the card it will generate a random number range 1 to 3
+4 matching the random number change that card with king and other cards with joker
+5 match user click and king card. 
+6 show message he wons or lost
+
+
+*/
--- JavaScript/Events/app.js
+++ JavaScript/Events/app.js
@@ -1,8 +1,137 @@
 
 
-var greetEl = document.getElementById('greeting');
+// var greetEl = document.getElementById('greeting');
 
-function greetUser() {
-    var userName = prompt('what is your name?');
-    greetEl.innerHTML = "Welcome " + userName;
-}
+// function greetUser() {
+//     var userName = prompt('what is your name?');
+//     greetEl.innerHTML = "Welcome " + userName;
+// }
+
+
+
+
+// var afEl = document.getElementById('pic');
+
+// function swapeImage(el) {
+//     // afEl.src = './images/2nd.jpg'
+//     // console.log(el);   
+
+//     el.src = './images/2nd.jpg';
+// }
+
+
+// function revertOriginalImage(el) {
+//     el.src = './images/afridi.jpg';
+// }
+
+
+
+
+
+
+// function changeStyle(h1) {
+
+//     h1.style.backgroundColor = "lightgreen"
+//     h1.style.fontSize = "40px"
+//     h1.style.color = "red"
+
+// }
+
+// function revertOriginalStyle(h1) {
+//     h1.style.backgroundColor = "white"
+//     h1.style.fontSize = "32px"
+//     h1.style.color = "black"
+
+// }
+
+
+
+
+// var detailedText = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium obcaecati consectetur nemo officia dignissimos, libero suscipit fugiat! Asperiores cumque corporis in possimus, fugiat eaque expedita consequatur recusandae quas veniam. Pariatur.'
+// var shortText = 'Lorem ipsum dolor, sit...'
+
+// var p = document.getElementById('loris');
+
+// function expand() {
+//     p.innerHTML = detailedText;
+// }
+
+// function compress() {
+//     p.innerHTML = shortText;
+// }
+
+
+
+
+
+
+
+
+
+// function changeStyle(el) {
+//     el.style.backgroundColor = 'lightgrey';
+//     el.style.color = 'green';
+// }
+
+// function revertOriginalStyle(el) {
+//     el.style.backgroundColor = 'white';
+//     el.style.color = 'black';
+// }
+
+
+
+
+
+
+// var provinceEl = document.getElementById('province');
+// var errorMsgEl = document.getElementById('error-msg');
+
+// function updateProvince(cityName) {
+//     if (cityName.value.toLowerCase() == 'karachi') {
+//         provinceEl.value = 'Sindh';
+//     }
+//     else if (cityName.value.toLowerCase() == 'lahore') {
+//         provinceEl.value = 'Punjab';
+//     }
+//     else if (cityName.value.toLowerCase() == 'peshawar') {
+//         provinceEl.value = 'KP';
+//     }
+//     else if (cityName.value.toLowerCase() == 'quetta') {
+//         provinceEl.value = 'Balochistan';
+//     }
+//     else {
+//         errorMsgEl.innerHTML = 'You must enter a capital city of Pakistan.'
+//     }
+
+//     var firstChr = cityName.value[0];
+//     firstChr = firstChr.toUpperCase();
+
+//     var restChr = cityName.value.slice(1);
+//     restChr = restChr.toLowerCase();
+
+//     var titleCaseCity = firstChr + restChr;
+//     cityName.value = titleCaseCity;
+// }
+
+
+
+
+
+
+
+
+
+
+// function changeStyle(h1) {
+//     h1.className += ' borders';
+
+// }
+
+
+
+
+
+
+
+
+
