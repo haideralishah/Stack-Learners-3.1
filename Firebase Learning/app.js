cb245109ccd3c85ee69aa3b879e21f9282367647
Haider Ali Shah <headeralishah@gmail.com>
2020-07-18 19:34:06 +0500

    Firebase firestore save data get once get realtime basic queries

--- Firebase Learning/app.js
+++ Firebase Learning/app.js
@@ -1,11 +1,13 @@
 var emailEl = document.getElementById('email');
 var passwordEl = document.getElementById('password');
-function signupUser() {
-    console.log(emailEl.value, passwordEl.value);
+var auth = firebase.auth();
+var db = firebase.firestore();
 
-    firebase.auth().createUserWithEmailAndPassword(emailEl.value, passwordEl.value)
+function signupUser() {
+    auth.createUserWithEmailAndPassword(emailEl.value, passwordEl.value)
         .then(function (success) {
             console.log(success, 'user registerd successfully.');
+            redirectToHome();
         })
         .catch(function (error) {
             console.log(error);
@@ -14,12 +16,106 @@ function signupUser() {
 
 
 function signin() {
-    console.log(emailEl.value, passwordEl.value);
-    firebase.auth().signInWithEmailAndPassword(emailEl.value, passwordEl.value)
+    auth.signInWithEmailAndPassword(emailEl.value, passwordEl.value)
         .then(function (user) {
             console.log('user', user.user.uid);
+            redirectToHome();
         })
         .catch(function (error) {
             console.log('error', error);
         })
-}   
+}
+
+
+function redirectToHome() {
+    localStorage.setItem('userInfo', JSON.stringify(auth.currentUser));
+    window.location.href = './home.html';
+}
+
+
+var todo = document.getElementById('todo-item');
+
+
+function addTodoItem() {
+    // console.log(auth.currentUser.uid);
+    db.collection("todo").add({
+        todo: todo.value,
+        uid: auth.currentUser.uid
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+            todo.value = '';
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+
+}
+
+
+// function getAllTodos() {
+//     db.collection("todo").get()
+//         .then((querySnapshot) => {            
+//             querySnapshot.forEach((doc) => {
+//                 console.log(doc.id, doc.data());
+//             });
+//         });
+// }
+
+// saveWithCustomeDocID();
+// function saveWithCustomeDocID(){
+//     db.collection('students').doc('javascript').set({
+//         studentName:'haider',
+//         roll: 2643,
+//         subject:'JS'
+//     })
+// }
+
+
+
+// function getCurrentUserTodos() {
+//     var uid = JSON.parse(localStorage.getItem('userInfo')).uid;
+
+//     db.collection("todo").where("uid", "==", uid)
+//         .get()
+//         .then((querySnapshot) => {
+//             querySnapshot.forEach((doc) => {
+//                 console.log(doc.id, doc.data());
+//             });
+//         });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+function getUserTodosRealtime() {
+    var uid = JSON.parse(localStorage.getItem('userInfo')).uid;
+    db.collection("todo").where("uid", "==", uid)
+        .onSnapshot(function (snapshot) {
+            snapshot.docChanges().forEach(function (change) {
+                if (change.type === "added") {
+
+                    console.log("New todo: ", change.doc.data());
+
+                }
+                // if (change.type === "modified") {
+
+                //     console.log("Modified city: ", change.doc.data());
+
+                // }
+                // if (change.type === "removed") {
+
+                //     console.log("Removed city: ", change.doc.data());
+
+                // }
+            });
+        })
+}
